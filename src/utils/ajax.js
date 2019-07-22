ffbaa3743adeb6c1fadebbd69d223edceb3626d0
zhangmanyi <will.up2018@foxmail.com>
2019-07-22 18:34:52 +0800

    ajax扩展Vue原型/eslint提醒修改

--- src/utils/ajax.js
+++ src/utils/ajax.js
@@ -1,15 +1,14 @@
-import axios from 'axios'
-export default{
-    $ajax:function(){
-        var p=new Promise((resove,reject)=>{
-            if(true){
-                resove('success')
-            }else{
-                resove('fail')
-            }
-        })
-        return p
-    }
-}
-
-
+import axios from 'axios';
+export default {
+  $ajax: function() {
+    var p = new Promise((resove, reject) => {
+      var one = true;
+      if (one) {
+        resove('success');
+      } else {
+        resove('fail');
+      }
+    });
+    return p;
+  }
+};
