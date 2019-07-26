e531985a7fe8b621a612b73e60556d82a63ec94d
zhangmanyi <will.up2018@foxmail.com>
2019-07-26 21:02:18 +0800

    1

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
-import Vue from 'vue';
-import './plugins/axios';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import './plugins/element.js';
-import './theme/index.css';
-import Ajax from './utils/ajax.js';
-import './style/reset.less';
-import './style/ele_reset.less';
+import Vue from "vue";
+import "./plugins/axios";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "./plugins/element.js";
+import "./theme/index.css";
+import Ajax from "./utils/ajax.js";
+import "./style/reset.less";
+import "./style/ele_reset.less";
 
 Vue.config.productionTip = false;
 Vue.prototype.$ajax = Ajax.$ajax;
@@ -16,13 +16,13 @@ new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app');
+}).$mount("#app");
 
 //路由跳转守卫判断是否是登录态
 router.beforeEach((to, from, next) => {
-  store.dispatch('enterRouter', to.path);
+  store.dispatch("enterRouter", to.path);
   if (store.state.readyOne == 1) {
-    store.dispatch('changeAppStatus', true);
+    store.dispatch("changeAppStatus", true);
   } else {
     next();
   }
