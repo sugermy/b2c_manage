6e249c1678fb5807f62ca895edcd9a05c2938131
zhangmanyi <will.up2018@foxmail.com>
2019-08-09 18:22:46 +0800

    1

--- src/router.js
+++ src/router.js
@@ -1,69 +1,69 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import Home from './views/Home';
+import Vue from "vue";
+import Router from "vue-router";
 //主页
-const HomeView = () => import('./views/Home/children/view');
-const Product = () => import('./views/Home/children/ProductList'); //产品列表
-const ProductDetail = () => import('./views/Home/children/ProductDetail');
-const OrderForm = () => import('./views/Home/children/OrderForm.vue'); //提交订单
+const Home = () => import("./views/home");
+const HomeView = () => import("./views/home/children/view");
+const Product = () => import("./views/home/children/ProductList"); //产品列表
+const ProductDetail = () => import("./views/home/children/ProductDetail");
+const OrderForm = () => import("./views/home/children/OrderForm"); //提交订单
 
 //个人中心
-const Personal = () => import('./views/Personal');
-const PersonalCenter = () => import('./views/Personal/children/PersonalCenter.vue');
-const PersonalOrder = () => import('./views/Personal/children/PersonalOrder.vue');
+const Personal = () => import("./views/personal");
+const PersonalCenter = () => import("./views/personal/children/PersonalCenter");
+const PersonalOrder = () => import("./views/personal/children/PersonalOrder");
 
 //游客须知
-const TouristRules = () => import('./views/TouristRules');
+const TouristRules = () => import("./views/touristRules/index");
 
 //联系我们
-const Contactus = () => import('./views/Contactus');
+const Contactus = () => import("./views/contactus/index");
 Vue.use(Router);
 
 export default new Router({
   routes: [
     //无路由默认跳转
     {
-      path: '/',
-      name: '',
-      redirect: '/Home/HomeView'
+      path: "/",
+      name: "",
+      redirect: "/Home/HomeView"
     },
     //首页
     {
-      path: '/Home',
-      name: 'Home',
+      path: "/Home",
+      name: "Home",
       component: Home,
-      redirect: '/Home/HomeView',
+      redirect: "/Home/HomeView",
       children: [
         {
-          path: 'HomeView',
-          name: 'HomeView',
+          path: "HomeView",
+          name: "HomeView",
           component: HomeView,
           meta: {
-            title: '首页'
+            title: "首页"
           }
         },
         {
-          path: 'Product',
-          name: 'Product',
+          path: "Product",
+          name: "Product",
           component: Product,
           meta: {
-            title: '产品列表'
+            title: "产品列表"
           }
         },
         {
-          path: 'Product/Detail',
-          name: 'ProductDetail',
+          path: "Product/Detail",
+          name: "ProductDetail",
           component: ProductDetail,
           meta: {
-            title: '产品详情'
+            title: "产品详情"
           }
         },
         {
-          path: 'Product/OrderForm',
-          name: 'OrderForm',
+          path: "Product/OrderForm",
+          name: "OrderForm",
           component: OrderForm,
           meta: {
-            title: '提交订单'
+            title: "提交订单"
           }
         }
       ]
@@ -71,33 +71,33 @@ export default new Router({
 
     //个人中心
     {
-      path: '/Personal',
-      name: 'Personal',
+      path: "/Personal",
+      name: "Personal",
       component: Personal,
-      redirect: '/Personal/PersonalCenter',
+      redirect: "/Personal/PersonalCenter",
       children: [
         {
-          path: 'PersonalCenter',
-          name: 'PersonalCenter',
+          path: "PersonalCenter",
+          name: "PersonalCenter",
           component: PersonalCenter
         },
         {
-          path: 'PersonalOrder',
-          name: 'PersonalOrder',
+          path: "PersonalOrder",
+          name: "PersonalOrder",
           component: PersonalOrder
         }
       ]
     },
     //游客须知
     {
-      path: '/TouristRules',
-      name: 'TouristRules',
+      path: "/TouristRules",
+      name: "TouristRules",
       component: TouristRules
     },
     //联系我们
     {
-      path: '/Contactus',
-      name: 'Contactus',
+      path: "/Contactus",
+      name: "Contactus",
       component: Contactus
     }
   ]
